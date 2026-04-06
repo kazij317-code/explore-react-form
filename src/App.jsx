720e79eb84057fc7570264ab67a76fabb5592a44
kazij317-code <kazij317@gmail.com>
2026-04-06 12:42:59 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+// -------------------------------Start: 38_1 ---------------------------------------
+// (1) st Clear inside content
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+  
+
+  return (
+    <>
+          <h1>Explore React Form</h1>
+          {/* (3) <SimpleForm+enter. then in SimpleForm.jsx file*/}
+          <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
+// (1) en Clear inside content. then create SimpleForm.jsx file creating SimpleForm folder creating components folder
+// -----------------------
+// Practice:
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+          <h1>Get started</h1>
+          <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
+
+// -------------------------------End:38_1-(1) to () --------------------------------
---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,82 @@
+// -------------------------------Start: 38_1 ---------------------------------------
+// (2) st rsc+enter
+import React from 'react';
+
+const SimpleForm = () => {
+    
+    // (6)st
+    // (9)handleSubmit = (e)
+    const handleSubmit = (e) =>{
+        // (10)
+        e.preventDefault();
+        // console.log('form submitted');
+        // (11)commit previous
+        console.log(e);
+        // (12)commit (11)
+        // console.log(e.target);
+        // (14)commit (12)
+        // console.log(e.target.name);
+        // (15)commit (14)
+        console.log(e.target.name.value);
+        // (17) then create __get_form_data.js file in src folder
+        console.log(e.target.email.value);
+
+
+    }
+    // (6)en
+
+    return (
+        <div>
+        {/* (4) st*/}
+        {/* (5)onSubmit={} */}
+        {/* (7) onSubmit={handleSubmit} */}
+        <form onSubmit={handleSubmit}>
+            {/* (8) placeholder='Your Name'*/}
+            {/* (13) name='name'*/}
+            <input type="text" name='name' placeholder='Your Name' />
+            <br />
+            {/* (16)st */}
+            <input type="email" name='email' placeholder='Your Email' />
+            <br />
+            {/* (16)en */}
+            {/* input:submit eita rakhte hobe*/}
+            <input type="submit" value="submit" />
+        </form>
+        {/* (4) en*/}
+        </div>
+    );
+};
+
+export default SimpleForm;
+// (2) en then in App.jsx file
+// -------------------
+// Practice:
+import React from 'react';
+
+const SimpleForm = () => {
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Your Name' />
+                <br />
+                <input type="submit" value="submit" />
+
+                <input type="email" name='email' placeholder='Your Email' />
+                <br />
+            </form>
+            
+            
+        </div>
+    );
+};
+
+export default SimpleForm;
+
+// -------------------------------End:38_1-(1) to () --------------------------------
